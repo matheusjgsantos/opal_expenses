cd5b37ea36884f4e354608401f3267311227e63c
Santos <gmatheus@8c8590ae227b.ant.amazon.com>
2018-06-22 23:51:58 +1000

    Added tkinter for graphics, fields, buttons and file browse code

--- opal_expenses_20180622.py
+++ opal_expenses_20180622.py
@@ -43,37 +43,12 @@ def sanitize():
 def openFileBrowser():
     from Tkinter import Tk
     import Tkinter, Tkconstants, tkFileDialog
-    from Tkinter import Entry, Button, Label, StringVar
     
-    #opalFileName=''
-    window = Tk()
-    window.title("OPAL Expenses PDF importer")
-    window.geometry('765x400')
-    textField=Label(window,width=13,anchor="w",borderwidth=1,text="OPAL expense file:")
-    textField.grid(column=1,row=0)
-    filenameFieldText=StringVar()
-    filenameField=Entry(window,width=60,textvariable=filenameFieldText)
-    filenameFieldText.set("No file selected")
-    filenameField.grid(column=2,row=0)
-    def pdfFileBrowse_func():
-        global opalFileName
-        opalFileName = tkFileDialog.askopenfilename(initialdir = "root", title = "Select Opal PDF report file",filetypes = (("PDF Files", "*.pdf"),("All files", "*.*")))
-        #filenameField.configure(width=60,anchor="w",borderwidth=1,text=opalFileName)
-        filenameFieldText.set(opalFileName)
-        #opal_formater(window.filename)
-    filenameBrowseButton=Button(window, text="Open PDF", command=pdfFileBrowse_func)
-    filenameBrowseButton.grid(column=3,row=0)
-    def processFileName_func():
-        #if opalFileName != '':
-        try:
-            opal_formater(opalFileName)
-        #else:
-            #filenameField.configure(width=90,anchor="w",borderwidth=1,text="Select a file to process")
-        except:
-            filenameFieldText.set("Select a file to process")
-    processFileButton=Button(window,text="Process",command=processFileName_func)
-    processFileButton.grid(column=3,row=1)
-    window.mainloop()
+    root = Tk()
+    root.title("OPAL Expenses PDF importer")
+    root.filename = tkFileDialog.askopenfilename(initialdir = "root", title = "Select Opal PDF report file",filetypes = (("PDF Files", "*.pdf"),("All files", "*.*")))
+    
+    return root.filename
 
 def pdf_to_csv(filename, separator, threshold):
     from cStringIO import StringIO
@@ -178,6 +153,6 @@ if __name__ == '__main__':
     # Call the formater function passing the input file
     #opal_formater(args.input)
     #try:
-    openFileBrowser()
+    opal_formater(openFileBrowser())
     #except:
     #    print "Canceled"
